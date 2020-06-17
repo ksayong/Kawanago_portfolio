862c90a9abb4b2a2281587830c1151058695c989
sayo <k.sayong86@gmail.com>
2020-06-17 22:57:55 +0900

    topに戻るアイコンを実装

--- js/index.js
+++ js/index.js
@@ -14,4 +14,22 @@ $(function(){
     $('.js_menu_mobile_toggle').on('click', function(){
         $('.js_head_nav_mobile').toggleClass('menu_mobile_show');
     });
+
+    //topに戻るアイコン
+    var topBtn = $('.top_icon');
+    topBtn.hide();
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 200) {
+            topBtn.fadeIn();
+        }else{
+            topBtn.fadeOut();
+        }
+    });
+    topBtn.click(function(){
+        $('body,html').animate({
+            scrollTop:0
+        }, 700);
+        return false;
+    })
+
 });
